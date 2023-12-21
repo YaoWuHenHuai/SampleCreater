bc154744466bb9b5c66bfd6abe76d475a4b09f06
Santi 电子档 <57055962+s8112441901@users.noreply.github.com>
2023-12-21 05:38:35 -0600

    Update SamplesCreater.py

--- SamplesCreater.py
+++ SamplesCreater.py
@@ -5,14 +5,62 @@ from datetime import date
 import numpy
 import random
 from timeit import default_timer as timer
+
+
+
 start = timer()
-
-
 
 #Creating DataForSample
 MainDirectory = (r"C:\Users\杜甫\Desktop\BaseQueryStructure")
 os.chdir(MainDirectory)
 print("Current directory is "+ MainDirectory)
+
+
+#Databases {AcceptanceReport, AllOperationData, PerformanceReport, TimelyUpdates}
+
+file_name = ""
+file_path = os.path.join(MainDirectory, file_name)
+
+#Define Function creates files, 
+def create_empty_dataframe(file_path):
+    createfiledf = pandas.DataFrame(columns= ["OperationId", "Date", "Customer", "CustomerID",
+                    "TransportationCompany", "TransportationCompanyID",
+                    "DepartureDate", "DeparturePlace", "ArrivingDate", "ArrivingPlace"],)
+    createfiledf.to_excel(file_name, index=False)
+
+
+#We Review by File name, case it exists pass, else will create file with the according columns 
+file_name = "AceptanceReport.xlsx"
+file_path = os.path.join(MainDirectory, file_name)
+if os.path.exists(os.path.join(file_path)):
+    print("already there")
+else:
+    create_empty_dataframe(file_path)
+
+
+file_name = "AllOperationData.xlsx"
+file_path = os.path.join(MainDirectory, file_name)
+if os.path.exists(os.path.join(file_path)):
+    print("already there")
+else:
+    create_empty_dataframe(file_path)
+
+
+file_name = "PerformanceReport.xlsx"
+file_path = os.path.join(MainDirectory, file_name)
+if os.path.exists(os.path.join(file_path)):
+    print("already there")
+else:
+    create_empty_dataframe(file_path)
+
+file_name = "TimelyUpdates.xlsx"
+file_path = os.path.join(MainDirectory, file_name)
+if os.path.exists(os.path.join(file_path)):
+    print("already there")
+else:
+    create_empty_dataframe(file_path)
+
+
 
 print("The following are the current files within the MainDirectory:")
 list = []
@@ -22,11 +70,12 @@ for x in os.listdir(MainDirectory):
 for x in enumerate(list):
     print(x)
 
-#Will each document at the directory  will create sample data information
+#Will each document at the directory  will add sample information within the files that are in there 
 for x in os.listdir(MainDirectory):
+    #double check
     if os.path.exists(os.path.join(MainDirectory, x)):
         print(x + " will be read")
-        df = pandas.read_excel(x)
+        df = pandas.read_excel(x, engine="openpyxl")
         print(df)
         column_names = df.columns.values.tolist()
         print(column_names)
@@ -60,7 +109,7 @@ for x in os.listdir(MainDirectory):
             }
             print(new_row_data)
             df.loc[len(df)] = new_row_data
-        #How many rows of sample we want, 
+        #How many rows of sample we want, in this case its 1000,
         for _ in range(1000):
             SampleCreator(df, today, CustomersList,conteo)
 
@@ -69,4 +118,5 @@ for x in os.listdir(MainDirectory):
         df.to_excel(x, index=False)
         end = timer()
 
+        print(end - start)  
         print(end - start)        
