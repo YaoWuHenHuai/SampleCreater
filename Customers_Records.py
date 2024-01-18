1719fe4e7e3690ec55af901bb57ba1bab59ce1d4
Santi 电子档 <57055962+YaoWuHenHuai@users.noreply.github.com>
2024-01-18 08:05:41 -0600

    Update Customers_Records.py
    * Fixed the new TodaysData row entry.
    * Now will ask the user case he wants to proceed

--- Customers_Records.py
+++ Customers_Records.py
@@ -3,6 +3,7 @@ import pandas
 from datetime import datetime
 import numpy
 from datetime import date
+import sys
 
 
 WorkingDirectory = (r"C:\Users\杜甫\Desktop\BaseQueryStructure\\")
@@ -14,21 +15,25 @@ TodaysDate = date.today()
 TodaysDate = TodaysDate.strftime("%m/%d/%Y")
 
 
-#Get customers_lists from records
+Continue = input("Continue? Yes/No:")
+if Continue.lower()=="yes":
+    pass
+else:
+    print("process stopped")
+    sys.exit()
+
+#Get customers_lists from records, this is for every customer has entered the system in total . 
 os.chdir(Data_Directory)
 reading_df = pandas.read_csv("lists.csv")
 Unique_Values_List = []
 for x in reading_df["Column"]:
     Unique_Values_List.append(x)
 
-
-
 #Case there is no dir, we create it. 
 if not os.path.exists(Clients_Records_Dir): 
     os.mkdir(Clients_Records_Dir)
     print(Clients_Records_Dir + " Created")
 os.chdir(Clients_Records_Dir)
-
 
 
 if os.path.exists(os.path.join(Clients_Records_Dir, Clients_Records_Excel_File)):
@@ -41,6 +46,10 @@ else:
     Dictionary = {"Date":[TodaysDate]}
     Customer_Record_df = pandas.DataFrame(Dictionary)
     print(Customer_Record_df)
+
+
+
+
 
 #Here we will make a directory out fo the distribution list part.
 Distribution_List_Folder = (r"C:\Users\杜甫\Desktop\Projects\ReportsAutomation\Distribution_List\\")
@@ -58,15 +67,18 @@ print(customers_list)
 
 
 #we add customer columns header case they dont exist, we verify from Distribution LIST.xlsx
+if TodaysDate not in Customer_Record_df["Date"].values:
+    Date_df = pandas.DataFrame({"Date": [TodaysDate]})
+    print(Date_df)
+    Customer_Record_df = Customer_Record_df._append(Date_df)
+
 
 for x in customers_list:
     if x not in Customer_Record_df.columns:
         Customer_Record_df.insert(1, x, [""])
 print(Customer_Record_df)
 
-#
 os.chdir(Clients_Records_Dir)
-
 
 #We iterraw specific column with todaysdata, so within there we notice for each customer didnt update information,.
 for index, row in Customer_Record_df.iterrows():
@@ -84,4 +96,3 @@ for index, row in Customer_Record_df.iterrows():
 
 Customer_Record_df.to_csv(Clients_Records_csv_File, index= False)
 Customer_Record_df.to_excel(Clients_Records_Excel_File, index= False)
-print(TodaysDate)
